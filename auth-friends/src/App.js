80a09182bd5c465dd4cbf16bf00c08a905539726
Ruth Poliakon <r.poliakon@gmail.com>
2019-11-03 17:29:58 -0700

    Login component form and route

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
 import "./App.css";
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>FriendsList</h1>
+      <nav>
+        <Link to="/login">Log In</Link>
+      </nav>
+
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
